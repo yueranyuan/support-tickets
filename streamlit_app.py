0237f1f1a54d07188c8dbfb3eefc52212e2715c8
Yueran (Aaron) Yuan <yueranyuan@gmail.com>
2024-09-15 17:17:31 +0000

    deploy check

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 # Show app title and description.
 st.set_page_config(page_title="Support tickets", page_icon="🎫")
-st.title("🎫 Support tickets")
+st.title("🎫 Support tickets 2")
 st.write(
     """
     This app shows how you can build an internal tool in Streamlit. Here, we are 
